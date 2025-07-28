da7c4e51615ef83229a9481f315fdd15d9e1b750
fghfthjtfj <Nik305demon@yandex.ru>
2025-07-28 14:37:00 +0300

    1.1

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.static_page, name='home'),
     path('about/', views.static_page, name='about'),
     path('catalog/', views.static_page, name='catalog'),
     path('contacts/', views.static_page, name='contacts'),
