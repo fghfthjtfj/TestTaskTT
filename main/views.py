76c08a706e0dec50e1b0cf244b3a64156a07ac7f
fghfthjtfj <Nik305demon@yandex.ru>
2025-07-28 15:09:26 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.urls import resolve
 from django.http import Http404
 from django.shortcuts import render
 
+# Словарь для отображаемого текста на странице
 page_titles = {
     'home': 'Главная',
     'about': 'О нас',
@@ -13,6 +14,7 @@ page_titles = {
 }
 
 
+# Используется 1 view, так как контент страницы одинаковый. В реальном проекте на каждую страницу может быть свой view
 def static_page(request):
     url_name = resolve(request.path).url_name
     if url_name not in page_titles:
